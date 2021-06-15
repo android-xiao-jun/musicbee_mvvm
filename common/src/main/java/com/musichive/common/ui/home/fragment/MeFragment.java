9252d176f58f65288f608f34c5315b3c6337dfb1
jun <15940363542@163.com>
2021-06-15 14:31:15 +0800

    自定义可拖拽进度条，（方便播放器扩展）

--- common/src/main/java/com/musichive/common/ui/home/fragment/MeFragment.java
+++ common/src/main/java/com/musichive/common/ui/home/fragment/MeFragment.java
@@ -1,10 +1,22 @@
 package com.musichive.common.ui.home.fragment;
 
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.kunminx.architecture.ui.page.DataBindingConfig;
 import com.musichive.common.BR;
 import com.musichive.common.R;
 import com.musichive.common.app.BaseStatusBarFragment;
 import com.musichive.common.ui.home.viewmodel.TempFragmentViewModel;
+import com.musichive.common.weight.PlayerSeekBar;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @Author Jun
@@ -23,7 +35,21 @@ public class MeFragment extends BaseStatusBarFragment {
 
     @Override
     protected DataBindingConfig getDataBindingConfig() {
-        return new DataBindingConfig(R.layout.common_fragment_me, BR.viewModel,homeFragmentViewModel);
+        return new DataBindingConfig(R.layout.common_fragment_me, BR.viewModel, homeFragmentViewModel);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //这里只是演示，，不要直接findviewbyid
+        PlayerSeekBar player_seeker_bar = getBinding().getRoot().findViewById(R.id.player_seeker_bar);
+//        player_seeker_bar.setSelectProgress(10);
+        player_seeker_bar.setOnProgressChangeListener(new PlayerSeekBar.OnProgressChangeListener() {
+            @Override
+            public void onChange(int selectProgress, boolean formUser) {
+                homeFragmentViewModel.indexStr.set("" + selectProgress);
+            }
+        });
     }
 
     @Override
